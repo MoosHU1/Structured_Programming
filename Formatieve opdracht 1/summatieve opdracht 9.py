87de770986bfd243abc62a68553a93488c465303
MoosHU1 <moos.boot@student.hu.nl>
2020-02-06 10:44:18 +0100

    test

---
+++ Formatieve opdracht 1/summatieve opdracht 9.py
@@ -0,0 +1,9 @@
+def move(ch, n):
+    if n>0:
+
+
+
+
+
+ch = int(input("Geef een byte: "))
+n = int(input("Hoeveel moet die verplaatst worden: "))
--- summatieve opdracht 2.py
+++
@@ -1,8 +0,0 @@
-str1 = input("Geef je string: ")
-str2 = input("Geef je string: ")
-
-for index in range(max(len(str1), len(str(2)))):
-    if str1[index] != str2[index]:
-        print("Het verschil zit bij index "+str(index))
-        break
-
