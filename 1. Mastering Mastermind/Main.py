8e72d637ea70371d58e636f805bf9692d95b0339
MoosHU1 <moos.boot@student.hu.nl>
2020-02-12 19:26:00 +0100

    12-02

--- 1. Mastering Mastermind/Main.py
+++ 1. Mastering Mastermind/Main.py
@@ -20,22 +20,32 @@ def random_color():
 
 def input_guess():
     guess_list = []
+    input_user_list = []
 
     input_user = input("Geef 4 kleuren op (Wit, Zwart, Blauw, Groen, Rood, Oranje) gescheiden door een komma:")
-    input_user_list = input_user.split(",")
+    list_colors = input_user.split(",")
+    for item in list_colors:
+        temp = (item.title()).strip()
+        input_user_list.append(temp)
 
     return input_user_list
 
+
 def player_guess():
     cpu_colors = [random_color(), random_color(), random_color(), random_color()]
-    print(cpu_colors)
+    #print(cpu_colors)
 
-    correct = False
-    while not correct:
+    beurt = 0
+
+    while True:
+        if beurt == 12:
+            print("Je beurten zijn op")
+            break
+
         black_pin = 0   # Goed op de goede plek
         white_pin = 0   # Goed op de verkeerde plek
         guess = input_guess()
-
+        print(guess)
         if guess == cpu_colors:
             print("Je hebt gewonnen!")
             break
@@ -48,13 +58,13 @@ def player_guess():
                 white_pin += 1
 
         print("{} Goede kleur en goede plek\n{} Goede kleur en verkeerde plek".format(black_pin, white_pin))
+        beurt += 1
+
+
+#def cpu_guess():
 
 
 
-# def cpu_guess():
-#
-#
-#
 
 
 
